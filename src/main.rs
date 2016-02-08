a212ad01052a9a08ba7f3ebde5767d5c67337bc5
Jenyay <jenyay.ilin@gmail.com>
2016-02-08 10:00:21 +0300

    Added errors output

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 extern crate citystreetlist;
 
 use citystreetlist::mosdata;
+use citystreetlist::mosdata::error;
+
+fn process_error (err: error::DownloadError) {
+    match err {
+        error::DownloadError::HttpError (_) => println!("Can't download data"),
+        error::DownloadError::Io (e) => println!("{:?}", e),
+        error::DownloadError::Zip (_) => println!("Can't extract data from zip archive"),
+    }
+}
 
 fn main () {
     match mosdata::download_areas() {
-        Err(e) => println!("{:?}", e),
+        Err(e) => process_error(e),
         Ok (res) => println!("{}", res),
     }
 }
