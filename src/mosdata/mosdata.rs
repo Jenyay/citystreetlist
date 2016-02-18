6924f211d615fcfcb607871a3fa089e8b3229989
Jenyay <jenyay.ilin@gmail.com>
2016-02-18 17:25:00 +0300

    Refactoring

--- src/mosdata/mosdata.rs
+++ src/mosdata/mosdata.rs
@@ -12,26 +12,29 @@ use self::hyper::header::Connection;
 
 use mosdata::error;
 
-
-pub fn download_areas () -> Result<String, error::DownloadError> {
-    const URL: &'static str = "http://data.mos.ru/opendata/export/2039/csv";
-
+fn get_zip_archive<'a> (url: &str) -> Result<zip::read::ZipArchive<io::Cursor<Vec<u8>>>, error::DownloadError> {
     // Download zip file with areas
     let client = Client::new();
 
-    let mut response = try! (client.get (URL).header (Connection::close()).send().map_err (error::DownloadError::HttpError));
+    let mut response = try! (client.get (url).header (Connection::close()).send().map_err (error::DownloadError::HttpError));
     let mut buffer: Vec<u8> = Vec::new();
     try! (response.read_to_end(&mut buffer).map_err (error::DownloadError::Io));
 
     // Extract file with areas
     let zip_cursor = io::Cursor::new(buffer);
-    let mut zip = try! (zip::ZipArchive::new (zip_cursor).map_err (error::DownloadError::Zip));
+    zip::ZipArchive::new (zip_cursor).map_err (error::DownloadError::Zip)
+}
+
+
+pub fn download_areas () -> Result<String, error::DownloadError> {
+    const URL: &'static str = "http://data.mos.ru/opendata/export/2039/csv";
+    let mut zip_archive = try! (get_zip_archive (URL));
 
     // Archive must contain single file only
-    assert_eq! (zip.len(), 1);
+    assert_eq! (zip_archive.len(), 1);
 
     // Extract data
-    let file = try! (zip.by_index (0).map_err (error::DownloadError::Zip));
+    let file = try! (zip_archive.by_index (0).map_err (error::DownloadError::Zip));
     let mut file_buffer = BufReader::new (file);
 
     loop {
